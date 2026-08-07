1db81300134932636f4352e3dbca0b86ac556eaf
Hifza Nazar <hifzanazar7575@gmail.com>
2026-08-07 11:47:59 +0500

    Fix Signup import path for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import SignUp from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
